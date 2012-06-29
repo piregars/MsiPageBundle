19e7a85ece36a5fe5e65cc9981d1b493d0f17a70
gnal <qwertypianox@gmail.com>
2012-06-29 09:52:08 -0400

    adding page entity

--- Entity/Page.php
+++ Entity/Page.php
@@ -20,12 +20,47 @@ class Page
     protected $id;
 
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\Column()
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column()
+     */
+    protected $slug;
+
+    /**
+     * @ORM\Column()
+     */
+    protected $layout;
+
+    /**
+     * @ORM\Column()
+     */
+    protected $author;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $keywords;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $published = false;
+
+    /**
+     * @ORM\Column(type="datetime", name="created_at")
      */
     protected $createdAt;
 
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="datetime", name="updated_at")
      */
     protected $updatedAt;
 
@@ -35,9 +70,96 @@ class Page
         $this->updatedAt = new \DateTime();
     }
 
-    public function ()
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function preUpdate()
     {
+        $this->updatedAt = new \DateTime();
+    }
 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getKeywords()
+    {
+        return $this->keywords;
+    }
+
+    public function setKeywords($keywords)
+    {
+        $this->keywords = $keywords;
+
+        return $this;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLayout()
+    {
+        return $this->layout;
+    }
+
+    public function setLayout($layout)
+    {
+        $this->layout = $layout;
+
+        return $this;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPublished()
+    {
+        return $this->published;
+    }
+
+    public function setPublished($published)
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
     }
 
     public function getId()
