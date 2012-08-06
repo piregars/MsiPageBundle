2045c219d5a7a4b9b89bcbac3193b248b1d96183
gnal <qwertypianox@gmail.com>
2012-08-06 19:18:02 -0400

    gg

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Doctrine\ORM\NonUniqueResultException;
 
 class PageController extends ContainerAware
 {
@@ -13,10 +14,14 @@ class PageController extends ContainerAware
     {
         $slug = $this->container->get('request')->attributes->get('slug');
 
-        if ($slug) {
-            $page = $this->container->get('msi_page_page_admin')->getModelManager()->findBy(array('a.enabled' => true, 't.slug' => $slug), array('a.blocks' => 'b'), array('b.position' => 'ASC'))->getQuery()->getOneOrNullResult();
-        } else {
-            $page = $this->container->get('msi_page_page_admin')->getModelManager()->findBy(array('a.enabled' => true, 'a.home' => true), array('a.blocks' => 'b'), array('b.position' => 'ASC'))->getQuery()->getOneOrNullResult();
+        try {
+            if ($slug) {
+                $page = $this->container->get('msi_page_page_admin')->getModelManager()->findBy(array('a.enabled' => true, 't.slug' => $slug), array('a.blocks' => 'b'), array('b.position' => 'ASC'))->getQuery()->getOneOrNullResult();
+            } else {
+                $page = $this->container->get('msi_page_page_admin')->getModelManager()->findBy(array('a.enabled' => true, 'a.home' => true), array('a.blocks' => 'b'), array('b.position' => 'ASC'))->getQuery()->getOneOrNullResult();
+            }
+        } catch (NonUniqueResultException $e) {
+
         }
 
         if (!$page) {
