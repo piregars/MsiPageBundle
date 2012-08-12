ad3fa3bc09d26cc61731570c51129b789eac65ea
gnal <qwertypianox@gmail.com>
2012-08-11 23:53:28 -0400

    upgrading to 2.1

--- Form/Type/PageTranslationType.php
+++ Form/Type/PageTranslationType.php
@@ -3,17 +3,25 @@
 namespace Msi\Bundle\PageBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class PageTranslationType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
             ->add('metaKeywords', 'textarea')
             ->add('metaDescription', 'textarea')
         ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Msi\Bundle\PageBundle\Entity\PageTranslation',
+        ));
     }
 
     public function getName()
